c07d3774b87becc30c0a233c09ee21c1f3c654a3
Matheus Altrao <matheusaltrao2@gmail.com>
2026-08-03 20:16:29 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@/components/ui/button'
+import { Text } from '@/components/ui/text'
+import { PortalHost } from '@rn-primitives/portal'
+import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import './global.css'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <View className="flex-1 items-center justify-center bg-background">
+        <Text className="mb-4 text-xl font-bold text-foreground">
+          Din Din App
+        </Text>
+        <Button>
+          <Text>Começar</Text>
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+      <PortalHost />
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
